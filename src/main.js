f787aa0739a775e17a1ac91bc72667a343425c9b
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-04-09 23:10:32 +0300

    отрисовка массива моковых данных
    На основе первого по порядку элемента в массиве отрисован компонент «Форма редактирования точки маршрута», а на основе оставшихся элементов отрисован компонент «Точка маршрута».

--- src/main.js
+++ src/main.js
@@ -5,12 +5,11 @@ import {createTripInfoTemplate} from './view/trip-info.js';
 import {createTripRouteTemplate} from './view/trip-route.js';
 import {createTripRoutePointElement} from './view/trip-route-point.js';
 import {createTripRouteEditPointTemplate} from './view/trip-route-edit-point.js';
-import {createTripRouteAddPointTemplate} from './view/trip-route-add-point.js';
 import {generateTripPoint} from './mock/trip-point.js';
 import {generateTripInfo} from './mock/trip-info.js';
 
 const POINT_COUNT = 20;
-
+const tripRoute = new Array(POINT_COUNT).fill().map(generateTripPoint);
 const siteHeaderElement = document.querySelector('.page-header');
 const siteNavigationElement = siteHeaderElement.querySelector('.trip-controls__navigation');
 const siteFiltersElement = siteHeaderElement.querySelector('.trip-controls__filters');
@@ -37,9 +36,13 @@ render(tripEventsElement, createTripRouteTemplate(), 'beforeend');
 
 const tripEventsListElement = tripEventsElement.querySelector('.trip-events__list');
 
-render(tripEventsListElement, createTripRouteEditPointTemplate(generateTripPoint()), 'beforeend');
-render(tripEventsListElement, createTripRouteAddPointTemplate(generateTripPoint()), 'beforeend');
+//render(tripEventsListElement, createTripRouteEditPointTemplate(generateTripPoint()), 'beforeend');
+//render(tripEventsListElement, createTripRouteAddPointTemplate(generateTripPoint()), 'beforeend');
 
-for(let i = 0; i< POINT_COUNT-1; i++) {
-  render(tripEventsListElement, createTripRoutePointElement(generateTripPoint()), 'beforeend');
+for(let i = 0; i< tripRoute.length-1; i++) {
+  if(i === 0) {
+    render(tripEventsListElement, createTripRouteEditPointTemplate(tripRoute[i]), 'beforeend');
+  } else {
+    render(tripEventsListElement, createTripRoutePointElement(tripRoute[i]), 'beforeend');
+  }
 }
