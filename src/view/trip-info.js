bcfd17fd3ad6c25a6bdba48c3723894e032f818e
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-04-10 19:23:12 +0300

    информация о дате начала и конца поездки вынесена в отдельную функцию

--- src/view/trip-info.js
+++ src/view/trip-info.js
@@ -3,7 +3,7 @@ import dayjs from 'dayjs';
 /**
  * Функция генерации общей информации о точках маршрута
  * @param {*} trip - точки, через которые проходит маршрут
- * @returns - строка с обхей информацией о точках маршрута
+ * @returns - строка с общей информацией о точках маршрута
  */
 const createTripInfoPoints = (trip) => {
   if(trip.length > 3){
@@ -16,6 +16,19 @@ const createTripInfoPoints = (trip) => {
 };
 
 /**
+ * Функция генерации информации и дате начала и конца поездки
+ * @param {*} dateTimeStart - дата начала поездки
+ * @param {*} dateTimeEnd - дата конца поездки
+ * @returns - строка с датами начала и конца поездки
+ */
+const createTripPeriod = (dateTimeStart, dateTimeEnd) => {
+  if(dayjs(dateTimeStart).month() === dayjs(dateTimeEnd).month()){
+    return `${dayjs(dateTimeStart).format('MMM DD')}&nbsp;&mdash;&nbsp;${dayjs(dateTimeEnd).format('DD')}`;
+  } else {
+    return `${dayjs(dateTimeStart).format('MMM DD')}&nbsp;&mdash;&nbsp;${dayjs(dateTimeEnd).format('MMM DD')}`;
+  }
+};
+/**
  * Функция создания разметки для блока с общей информацией о маршруте
  * @param {object} tripInfo - объект содержащий данные с общей информацией и маршруте
  * @returns - строка, содержащая разметку для блока с общей информацией и маршруте
@@ -26,7 +39,7 @@ export const createTripInfoTemplate = (tripInfo) => {
   <div class="trip-info__main">
     <h1 class="trip-info__title">${createTripInfoPoints(trip)}</h1>
 
-    <p class="trip-info__dates">${dayjs(dateTimeStart).format('MMM DD')}&nbsp;&mdash;&nbsp;${(dayjs(dateTimeStart).month() === dayjs(dateTimeEnd).month())?dayjs(dateTimeEnd).format('DD'):dayjs(dateTimeEnd).format('MMM DD')}</p>
+    <p class="trip-info__dates">${createTripPeriod(dateTimeStart, dateTimeEnd)}</p>
   </div>
   <p class="trip-info__cost">
     Total: &euro;&nbsp;<span class="trip-info__cost-value">${totalPrice}</span>
