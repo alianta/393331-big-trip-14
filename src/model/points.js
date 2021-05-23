7fdd579f283fe6044426940950a3f6b841b6f613
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-05-23 22:36:39 +0300

    добавлены методы паттерна Адаптер

--- src/model/points.js
+++ src/model/points.js
@@ -53,4 +53,56 @@ export default class Points extends Observer {
 
     this._notify(updateType);
   }
+
+  static adaptToClient(point) {
+    const adaptedPoint = Object.assign(
+      {},
+      point,
+      {
+        dateTimeStart: point.date_from !== null ? new Date(point.date_from) : point.date_from,
+        dateTimeEnd: point.adaptedPoint !== null ? new Date(point.date_to) : point.date_to,
+        price: point.base_price,
+        isFavorite: point.is_favorite,
+        offers: point.offers.map((item) => ({name: item.title, price: item.price})),
+        destinationDetails: {
+          description: point.destination.description,
+          photos: point.destination.pictures.map((item) => item.src),
+        },
+        destination: point.destination.name,
+      },
+    );
+
+    delete adaptedPoint.date_from;
+    delete adaptedPoint.date_to;
+    delete adaptedPoint.base_price;
+    delete adaptedPoint.is_favorite;
+
+    return adaptedPoint;
+  }
+
+  static adaptToServer(point) {
+    const adaptedPoint = Object.assign(
+      {},
+      point,
+      {
+        'date_from': point.dateTimeStart instanceof Date ? point.dateTimeStart.toISOString() : null,
+        'date_to': point.dateTimeEnd instanceof Date ? point.dateTimeEnd.toISOString() : null,
+        'base_price': point.price,
+        'is_favorite': point.isFavorite,
+        'offers': point.offers.map((item) => ({title: item.name, price: item.price})),
+        'destination': {
+          'description': point.estinationDetails.description,
+          'name': point.destination,
+          'pictures': point.destinationDetails.photos.map((item) => ({'src': item, 'description': ''})),
+        },
+      },
+    );
+
+    delete adaptedPoint.dateTimeStart;
+    delete adaptedPoint.dateTimeEnd;
+    delete adaptedPoint.price;
+    delete adaptedPoint.isFavorite;
+
+    return adaptedPoint;
+  }
 }
