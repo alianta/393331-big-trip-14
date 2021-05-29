758f442fb613ec54892841a19a540bf97c97650f
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-05-29 12:00:07 +0300

    исправлена фильтрация (за один вызов filter)

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -2,14 +2,16 @@ import {FilterType} from '../const.js';
 import dayjs from 'dayjs';
 
 const getPastPoints = (points) => {
-  const pastPoints = points.filter((point) =>  dayjs().isAfter(dayjs(point.dateTimeEnd)));
-  const futureAndPastPoints = points.filter((point) =>  {return dayjs().isBefore(dayjs(point.dateTimeEnd)) && dayjs().isAfter(dayjs(point.dateTimeStart)); });
-  return pastPoints.concat(futureAndPastPoints);
+  const pastPoints =  points.filter((point) =>  {
+    return dayjs().isBefore(dayjs(point.dateTimeEnd)) && dayjs().isAfter(dayjs(point.dateTimeStart)) || dayjs().isAfter(dayjs(point.dateTimeEnd));
+  });
+  return pastPoints;
 };
 const getFuturePoints = (points) => {
-  const futurePoints = points.filter((point) =>  !dayjs().isAfter(dayjs(point.dateTimeStart)));
-  const futureAndPastPoints = points.filter((point) =>  {return dayjs().isBefore(dayjs(point.dateTimeEnd)) && dayjs().isAfter(dayjs(point.dateTimeStart)); });
-  return futurePoints.concat(futureAndPastPoints);
+  const futurePoints = points.filter((point) =>  {
+    return dayjs().isBefore(dayjs(point.dateTimeEnd)) && dayjs().isAfter(dayjs(point.dateTimeStart)) || !dayjs().isAfter(dayjs(point.dateTimeStart));
+  });
+  return futurePoints;
 };
 
 export const filter = {
