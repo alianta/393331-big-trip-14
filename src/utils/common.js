b5fdf6d7075961cc1ec479a920af074322ed4ad0
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-05-29 14:15:14 +0300

    удалено неиспользуемое

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,33 +1,6 @@
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 dayjs.extend(duration);
-import {RANDOM_MIN_DAY, RANDOM_MAX_DAY, RANDOM_MIN_TIME, RANDOM_MAX_TIME} from '../const.js';
-/**
- * Функция из интернета по генерации случайного числа из диапазона
- * Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
- * @param {Integer} a - начало диапазона
- * @param {Integer} b -конец диапазона
- * @returns - случайное чило из диапазона от a до b
- */
-export const getRandomInteger = (a = 0, b = 1) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-
-  return Math.floor(lower + Math.random() * (upper - lower + 1));
-};
-
-/**
- * Функция генерация случайной даты со временем
- * @returns - случайная дата и время
- */
-export const getRandomDayAndTime = (startDate = null) => {
-  const randomDate = getRandomInteger(RANDOM_MIN_DAY, RANDOM_MAX_DAY);
-  const randomTime = getRandomInteger(RANDOM_MIN_TIME, RANDOM_MAX_TIME);
-  if(startDate === null) {
-    return dayjs().add(randomDate, 'day').add(randomTime, 'minute').toDate();
-  }
-  return dayjs(startDate).add(randomDate, 'day').add(randomTime, 'minute').toDate();
-};
 
 export const isPointsEqual = (pointA, pointB) => {
 
--- src/utils/trip.js
+++ src/utils/trip.js
@@ -1,42 +1,5 @@
-import {getRandomInteger} from './common.js';
-import {DESTINATIONS, TYPES, DESTINATION_DESCRIPTIONS, MAX_NUMBER_PHOTO} from '../const.js';
+import {TYPES} from '../const.js';
 import dayjs from 'dayjs';
-
-/**
- * Функция генерации случайного напрвления для точки маршрута
- * @returns - строка с названием направления
- */
-export const getRandomDestination = () => {
-  const randomIndex = getRandomInteger(0, DESTINATIONS.length - 1);
-  return DESTINATIONS[randomIndex];
-};
-
-/**
- * Функция генерации случайного типа точки маршрута
- * @returns - строка с названием типа маршрута
- */
-export const getRandomPointType = () => {
-  const randomIndex = getRandomInteger(0, TYPES.length - 1);
-  return TYPES[randomIndex].name;
-};
-
-/**
- * Функция генерации случайного описания о месте назначения точки маршрута
- * @returns  - строка с описанием места назначения
- */
-export const getRandomDestinationDescription = () => {
-  const randomIndex = getRandomInteger(0, DESTINATION_DESCRIPTIONS.length - 1);
-  return DESTINATION_DESCRIPTIONS[randomIndex];
-};
-
-/**
- * Функция генерация адреса случайной фотографии
- * @returns - строка с адресом случайного фото
- */
-export const generatePhoto = () => {
-  const randomIndex = getRandomInteger(0, MAX_NUMBER_PHOTO);
-  return `http://picsum.photos/248/152?r=${randomIndex}`;
-};
 
 export const sortTime = (pointA, pointB) => {
   const durationPointA = dayjs.duration(dayjs(pointA.dateTimeEnd).diff(dayjs(pointA.dateTimeStart)));
